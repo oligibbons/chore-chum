ae0ffff3382197a2eb4cef2c958fa0e9cd5b6ae4
Oliver Perkins-Gibbons <oliverperkins-gibbons@Olivers-MacBook-Pro.local>
2025-11-19 21:37:52 +0000

    Removed experimental React compiler

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    reactCompiler: true,
-  },
+  /* config options here */
+  // reactCompiler: true, <--- Removed this line to fix the build error
 };
 
 export default nextConfig;
